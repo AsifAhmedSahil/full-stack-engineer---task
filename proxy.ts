fc9d954325a5f226cf331db2418dff66e45da165
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-04-05 17:30:47 +0600

    proxy change for privacy

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-
 const protectedPaths = ['/feed']
+const authPaths = ['/login', '/register']
 
 export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl
-
-  const isProtected = protectedPaths.some((p) => pathname.startsWith(p))
-  if (!isProtected) return NextResponse.next()
-
   const token = req.cookies.get('auth_token')?.value
-  // We only check if the token exists here. 
-  // The actual JWT signature verification is done securely in the Server Components
-  // (Node.js runtime) because 'jsonwebtoken' does not run on the Edge runtime.
-  if (!token) {
+
+  const isProtected = protectedPaths.some((p) =>
+    pathname.startsWith(p)
+  )
+
+  const isAuthPage = authPaths.some((p) =>
+    pathname.startsWith(p)
+  )
+
+  // ❌ Not logged in → block protected pages
+  if (isProtected && !token) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
+
+  // ❌ Already logged in → block login/register
+  if (isAuthPage && token) {
+    return NextResponse.redirect(new URL('/feed', req.url))
+  }
+
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/feed/:path*'],
+  matcher: ['/feed/:path*', '/login', '/register'],
 }
